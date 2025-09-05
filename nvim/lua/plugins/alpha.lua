57971b90b95346e0f999f8b4d1aa69bc049eb2e6
Sébastien Doussang <sebastien.doussang@epitech.eu>
2025-09-05 16:27:50 +0200

    fix: changed alpha shortcut

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -53,7 +53,7 @@ return {
         dashboard.section.buttons.val = {
             dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
             dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
-            dashboard.button("g", "  Find text", ":Telescope live_grep <CR>"),
+            dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
             dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
             dashboard.button("c", "  Configuration", ":e ~/.config/nvim<CR>"),
             dashboard.button("l", "  Lazy", ":Lazy<CR>"),
