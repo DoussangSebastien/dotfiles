793bb3c67291f787270ddad486c9996ba3780789
Sébastien Doussang <sebastien.doussang@epitech.eu>
2025-09-12 16:09:40 +0200

    feat: clipThat plugin

---
+++ nvim/lua/plugins/clipThat.lua
@@ -0,0 +1,10 @@
+return {
+    -- dir="~/perso/clipThat/",
+    "DoussangSebastien/clipThat.nvim",
+    opts = {
+        max_history = 7,
+        default_keybind = true,
+        paste_mode = true,
+        copy_mode = true,
+    }
+}
